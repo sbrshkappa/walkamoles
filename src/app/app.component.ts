c2994643ad19cfa9b52c73f62c3c6afef3d4950f
Sabareesh Kappagantu <skappagantu@osisoft.com>
2021-02-03 15:20:30 -0800

    Added basic signup and login capability for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  email: string;
+  password: string;
   title = 'walkamoles';
+
+  constructor(public authService: AuthService){
+
+  }
+
+  signup() {
+    this.authService.signup(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  login() {
+    this.authService.login(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
